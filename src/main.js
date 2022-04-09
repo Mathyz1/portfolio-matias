c9448471a67bb5f8108157b4d26b64910f4b3237
Mathyz1 <49544505+Mathyz1@users.noreply.github.com>
2022-04-09 14:43:21 -0300

    menu desplegable

--- src/main.js
+++ src/main.js
@@ -1,1 +1,8 @@
 //https://github.com/Mathyz1/portfolio-matias
+
+const btnMenu = document.querySelector("#btnMenu");
+const optionsMenu = document.querySelector("nav ul");
+
+btnMenu.addEventListener("click", e =>{
+    optionsMenu.classList.toggle("active");
+});
